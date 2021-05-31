2a41fd2a93ed82f6b0992a186e09bbac9911b0d1
SamuelDMQ-DEV <69172972+SamuelDMQ-DEV@users.noreply.github.com>
2021-05-31 05:01:10 -0400

    Eliminar Comentarios

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
 require_once 'inc/iniciar_conexion.php';
 require_once 'inc/all_define.php';
-//require_once 'inc/dolar_today.php';  
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -17,14 +16,7 @@ require_once 'inc/all_define.php';
     <div class="contenedor">
         <div class="top">
            <h1 class="title"><?php echo TITLE_FOR_PAG;?></h1>
-		   <hr>
-            <!--<div class="menu">
-			<?php 
-                for($i = 0; $i <sizeof($menu); $i++) {
-                    echo '<a onclick="DisplayOption('.($i+1).','.sizeof($menu).')">'.$menu[$i].'</a>';
-                }
-             ?>
-            </div>-->
+		<hr>
         </div>
         <div class="menu">
 			<form class="item_search" action="">
@@ -32,14 +24,6 @@ require_once 'inc/all_define.php';
 			</form>
 			<div id="menu-pagination">
 			<?php include 'inc/items_pag.php';?>    
-            <!--<div id="option_1">
-            </div>-->
-            <!--<div id="option_2">
-                <h1>hola 2</h1>                
-            </div>
-            <div id="option_3">
-                <h1>hola 3</h1>                
-            </div>-->
 			</div>
         </div>
         <div class="spam">
@@ -51,42 +35,5 @@ require_once 'inc/all_define.php';
             <p>© copyright | <?php echo TITLE_FOR_PAG;?></p>
         </footer>
     </div>
-    <script type="text/javascript">
-        /*function DisplayOption(id, max_size) {
-            var name_id;
-            var x;
-            for (var i = 0; i <max_size; i++) {
-                var real_id = i+1;
-                if (id != real_id) {
-                    name_id = 'option_' + real_id;
-                    x = document.getElementById(name_id);
-                    if (x.style.display !== "none") x.style.display = "none";
-                }
-            }
-            name_id = 'option_' + id;
-            x = document.getElementById(name_id);
-            x.style.display = "block";
-        }*/
-        /*$(document).ready(function() {	
-            $('.pagination a').on('click', function(){
-                $('.items').html('<div class="loading"><img src="https://acegif.com/wp-content/uploads/loading-25.gif" width="70px" height="70px"/><br/>Un momento por favor...</div>');
-
-                var page = $(this).attr('data');		
-                var dataString = 'page='+page;
-                
-                $.ajax({
-                    type: "GET",
-                    url: "inc/ajax.php",
-                    data: dataString,
-                    success: function(data) {
-                        $('.items').fadeIn(2000).html(data);
-                        $('.pagination a').removeClass('active');
-                        $('.pagination a[data="'+page+'"]').addClass('active');
-                    }
-                });
-                return false;
-            });              
-        });*/  
-    </script>
 </body>
 </html>
